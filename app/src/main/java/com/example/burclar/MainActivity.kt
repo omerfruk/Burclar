a8c8aff8a4d38762ae1531eeb8789666978125bc
serhatkarakoca <serhatkarakoca@gmail.com>
2020-02-14 14:37:29 +0300

    SignArrayAdapter.kt updated

--- app/src/main/java/com/example/burclar/MainActivity.kt
+++ app/src/main/java/com/example/burclar/MainActivity.kt
@@ -8,18 +8,30 @@ import kotlinx.android.synthetic.main.activity_main.*
 class MainActivity : AppCompatActivity() {
 
 
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         var signs = resources.getStringArray(R.array.burclar)
         var signDate = resources.getStringArray(R.array.burctarihleri)
-        var signImages = arrayOf(R.drawable.aries,R.drawable.taurus,R.drawable.gemini,R.drawable.cancer,R.drawable.leo,R.drawable.virgo,
-            R.drawable.libra,R.drawable.scorpio,R.drawable.sagittarius,R.drawable.capricorn,R.drawable.aquarius,R.drawable.pisces)
+        var signImages = arrayOf(
+            R.drawable.aries,
+            R.drawable.taurus,
+            R.drawable.gemini,
+            R.drawable.cancer,
+            R.drawable.leo,
+            R.drawable.virgo,
+            R.drawable.libra,
+            R.drawable.scorpio,
+            R.drawable.sagittarius,
+            R.drawable.capricorn,
+            R.drawable.aquarius,
+            R.drawable.pisces
+        )
 
-       // var myAdapter = ArrayAdapter<String>(this,R.layout.teksatir,R.id.tvSignName,signs)
-        var myAdapter = SignArrayAdapter(this,R.layout.teksatir,R.id.tvSignName,signs,signDate,signImages)
-        listSigns.adapter =myAdapter
+        // var myAdapter = ArrayAdapter<String>(this,R.layout.teksatir,R.id.tvSignName,signs)
+        var myAdapter =
+            SignArrayAdapter(this, R.layout.teksatir, R.id.tvSignName, signs, signDate, signImages)
+        listSigns.adapter = myAdapter
     }
 }
--- app/src/main/java/com/example/burclar/SignArrayAdapter.kt
+++ app/src/main/java/com/example/burclar/SignArrayAdapter.kt
@@ -1,14 +1,37 @@
 package com.example.burclar
 
 import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import kotlinx.android.synthetic.main.teksatir.view.*
+import java.util.zip.Inflater
 
 class SignArrayAdapter(
     context: Context,
     resource: Int,
     textViewResourceId: Int,
-    signNames: Array<String>,
-    signDates:Array< String>,
-    signImages : Array<Int>
+    var signNames: Array<String>,
+    var signDates: Array<String>,
+    var signImages: Array<Int>
 ) : ArrayAdapter<String>(context, resource, textViewResourceId, signNames) {
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+
+        var inflater = LayoutInflater.from(context)
+        var singleLineLayout = inflater.inflate(R.layout.teksatir, parent, false)
+
+        var sign_name = singleLineLayout.tvSignName
+        var sign_date = singleLineLayout.tvSignDate
+        var sign_img = singleLineLayout.imgSign
+
+        sign_name.setText(signNames[position])
+        sign_date.setText(signDates[position])
+        sign_img.setImageResource(signImages[position])
+
+
+        return singleLineLayout
+
+    }
 }
