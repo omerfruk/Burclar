fef87bf76b81310701af4f939f9cac5c740e9eaf
serhatkarakoca <serhatkarakoca@gmail.com>
2020-03-21 13:20:30 +0300

    initial commit

--- app/src/main/java/com/example/burclar/DetailActivity.kt
+++ app/src/main/java/com/example/burclar/DetailActivity.kt
@@ -1,8 +1,11 @@
 package com.example.burclar
 
+import android.graphics.BitmapFactory
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import androidx.palette.graphics.Palette
+import kotlinx.android.synthetic.main.activity_detail.*
 
 class DetailActivity : AppCompatActivity() {
 
@@ -10,9 +13,36 @@ class DetailActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detail)
 
-        var position = intent.extras?.get("position")
+        var position = intent.extras?.get("position") as Int
         var allSignsData = intent.extras!!.get("allSignsData") as ArrayList<Signs>
-        Toast.makeText(this, "position : "+ position+"All"+allSignsData.size,Toast.LENGTH_LONG).show()
+        //Toast.makeText(this, "position : "+ position+"All"+allSignsData.size,Toast.LENGTH_LONG).show()
+        tvSignFeature.setText(allSignsData.get(position).signDetails)
+        header.setImageResource(allSignsData.get(position).signImgBig)
 
+        setSupportActionBar(anim_toolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        collapsing_toolbar.title = allSignsData.get(position).signName
+
+        var bitmap = BitmapFactory.decodeResource(resources,allSignsData.get(position).signImgBig)
+
+        Palette.from(bitmap).generate(object : Palette.PaletteAsyncListener{
+            override fun onGenerated(palette: Palette?) {
+                var color = palette?.getVibrantColor(R.attr.colorAccent)
+                collapsing_toolbar.setContentScrimColor(color!!)
+
+                window.statusBarColor = color
+            }
+
+
+        })
+
+
+
+
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        onBackPressed()
+        return super.onSupportNavigateUp()
     }
 }
